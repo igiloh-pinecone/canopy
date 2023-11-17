bf5f1c12dbff0534c225a184e2047d4d73356cd6
aulorbe <aulorbe@gmail.com>
2023-11-17 15:37:23 -0800

    For roie

--- src/canopy/llm/azure_openai_llm.py
+++ src/canopy/llm/azure_openai_llm.py
@@ -9,12 +9,15 @@ class AzureOpenAILLM(OpenAILLM):
     """
     Azure OpenAI LLM wrapper built on top of the OpenAI Python client.
 
-    Note: Azure OpenAI services requires a valid Azure API key and a valid Azure endpoint to use this class.
+    Note: Azure OpenAI services requires an Azure API key, Azure endpoint, and OpenAI API version.
+
+    a valid Azure API key and a valid Azure endpoint to use this class.
           You can set the "AZURE_OPENAI_KEY" and "AZURE_OPENAI_ENDPOINT" environment variables to your API key and
           endpoint, respectively, or you can directly, e.g.:
           >>> from openai import AzureOpenAI
           >>> AzureOpenAI.api_key = "YOUR_AZURE_API_KEY"
           >>> AzureOpenAI.api_base = "YOUR_AZURE_ENDPOINT"
+          >>> AzureOpenAI.api_version = "THE_OPENAI_API_VERSION_YOU_ARE_USING"
 
     Note: If you want to pass an OpenAI organization, you need to set an environment variable "OPENAI_ORG_ID". Note
           that this is different from the environment variable name for passing an organization to the parent class,
@@ -24,17 +27,17 @@ class AzureOpenAILLM(OpenAILLM):
     """
     def __init__(self,
                  *,
-                 azure_api_version: str,
-                 model_name: str,
-                 azure_api_key: Optional[str] = None,
+                 model_name: Optional[str] = None,
                  base_url: Optional[str] = None,
-                 **kwargs: Any
+                 azure_api_version: Optional[str] = None,
+                 azure_api_key: Optional[str] = None,
+                 **kwargs: Any,
                  ):
         """
         Initialize the AzureOpenAI LLM.
 
         Args:
-            azure_api_version: The Aure OpenAI API version, e.g. "2023-05-15".
+            azure_api_version: The Auzre OpenAI API version, e.g. "2023-05-15".
                       Find versions here: https://learn.microsoft.com/en-us/rest/api/azureopenai/files
             model_name: The name of the deployed Azure model you are connecting to. This is *not* the name of the
                       OpenAI LLM underlying your deployed Azure model.
@@ -46,25 +49,25 @@ class AzureOpenAILLM(OpenAILLM):
                       `enforced_function_call` methods.
         """
         super().__init__(model_name)
-        self.azure_api_version = azure_api_version
-        self.model_name = model_name
-        self.azure_api_key = azure_api_key
-        self.base_url = base_url
+        # self.model_name = model_name
+        # self.base_url = base_url
+        # self.azure_api_version = azure_api_version
+        # self.azure_api_key = azure_api_key
 
-        self._client = AzureOpenAI(azure_endpoint=self.base_url,
-                                   api_key=self.azure_api_key,
-                                   api_version=self.azure_api_version
-                                   )
+        self._client = AzureOpenAI(azure_deployment=model_name,
+                                   azure_endpoint=base_url,
+                                   api_version=azure_api_version,
+                                   api_key=azure_api_key)
 
         self.default_model_params = kwargs
 
 
-if __name__ == "__main__":
-    AZURE_OPENAI_ENDPOINT = "https://devrel.openai.azure.com/"
-    AZURE_OPENAI_KEY = "d5e870aabbb14cb09a52038215c0fb37"
-    llm = AzureOpenAILLM(azure_api_version="2023-07-01-preview",
-                         azure_api_key=AZURE_OPENAI_KEY,
-                         base_url=AZURE_OPENAI_ENDPOINT,
-                         model_name="audrey_canopy_test")
+# if __name__ == "__main__":
+#     AZURE_OPENAI_ENDPOINT = "https://devrel.openai.azure.com/"
+#     AZURE_OPENAI_KEY = ""
+#     llm = AzureOpenAILLM(azure_api_version="2023-07-01-preview",
+#                          azure_api_key=AZURE_OPENAI_KEY,
+#                          base_url=AZURE_OPENAI_ENDPOINT,
+#                          model_name="audrey_canopy_test")
 
-    print('hi')
+    # print('hi')
